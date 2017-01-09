2481c9014c20b9ab56c189819b7612e50ef77173
Sarath <sarathiscookie@hotmail.com>
2017-01-09 22:55:31 +0530

    User migration file updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,9 +15,12 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
+            $table->string('name')->unique();
             $table->string('email')->unique();
             $table->string('password');
+            $table->string('slug');
+            $table->boolean('gender');
+            $table->string('avatar');
             $table->rememberToken();
             $table->timestamps();
         });
