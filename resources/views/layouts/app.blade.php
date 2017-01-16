d2f3e9f1c378148ead151f7587b8093f27cfecf9
Sarath <sarathiscookie@hotmail.com>
2017-01-16 23:25:02 +0530

    Social-2: Profile page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,6 +60,7 @@
 
                                 <ul class="dropdown-menu" role="menu">
                                     <li>
+                                        <a href="{{ url('/profile') }}">Profile</a>
                                         <a href="{{ url('/logout') }}"
                                             onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
